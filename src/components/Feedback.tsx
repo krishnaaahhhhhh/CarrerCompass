7e87e41d7b0004d47f296c1c69c5f774edd97252
krishnaaahhhhhh <Krishnasinghrathour9628@gmail.com>
2026-05-01 11:35:39 +0530

    feat: finalize emailjs variable mapping

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -27,9 +27,9 @@ export default function Feedback() {
         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
         process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
         {
-          from_name: data.name,
-          from_email: data.email,
-          phone_number: data.phone,
+          name: data.name,
+          email: data.email,
+          phone: data.phone,
           message: data.query,
         },
         process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || ""
